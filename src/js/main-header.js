deaafd5e9203ce29925cf319d9bc54750a158d64
박상희 <9011155@naver.com>
2025-03-24 17:23:39 +0900

    :recycle: refactor: modal 제작 및 추가 / 컴포넌트 페이지들 리팩토링

--- src/js/main-header.js
+++ src/js/main-header.js
@@ -254,6 +254,23 @@ class mainHeader extends HTMLElement {
 		});
 
 		
+		document.addEventListener('DOMContentLoaded', function() {
+			const menuButton = document.querySelector('.menu-mobile .bi-list').closest('div');
+			const menuLists = document.querySelector('.menu-lists');
+			const mobileMenu = document.querySelector('.mobile-menu');
+
+			menuButton.addEventListener('click', function() {
+				mobileMenu.classList.remove('-left-[100%]');
+				mobileMenu.classList.add('left-0');
+			});
+			
+			menuLists.addEventListener('click', function() {
+				mobileMenu.classList.remove('left-0');
+				mobileMenu.classList.add('-left-[100%]');
+			});
+		});
+
+		
 	}
 }
 
--- src/main.js
+++ src/main.js
@@ -2,19 +2,3 @@ import "./style.css"
 import "./js/main-header.js"
 import "./js/sub-header.js"
 import "./js/footer.js"
-
-document.addEventListener('DOMContentLoaded', function() {
-  const menuButton = document.querySelector('.menu-mobile .bi-list').closest('div');
-  const menuLists = document.querySelector('.menu-lists');
-  const mobileMenu = document.querySelector('.mobile-menu');
-
-  menuButton.addEventListener('click', function() {
-    mobileMenu.classList.remove('-left-[100%]');
-    mobileMenu.classList.add('left-0');
-  });
-  
-  menuLists.addEventListener('click', function() {
-    mobileMenu.classList.remove('left-0');
-    mobileMenu.classList.add('-left-[100%]');
-  });
-});
